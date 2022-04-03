272972ec99010dcec3c1a52c9e86b6f955c99816
Daniel Xutuc <danielxutuc@gmail.com>
2022-04-03 12:25:02 -0300

    primer commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# react with ant design boilerplate
+
+
+Para instalar descargar el repositorio con git.
+
+Ejecutar
+```
+npm install
+```
+
+después de instalar todos los paquetes para ejecutar usar
+
+```
+npm start
+```
+
+Empezar a programar!!!
+
+
+## Descripcion de lo que hacen los anteriore comandos
+El primero se encarga de traer todas las librerias y dependencias
+El segundo se encarga de levantar un servidor de desarrollo que está atento a cualquier cambio de código para compilar el html y javascript final. Que se despliega en el servidor de desarrollo en localhost.
+
+
+ 
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import { DatePicker } from 'antd';
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+import Sider from './Sider.js'
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DatePicker />
+        <Sider />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to recharge.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
